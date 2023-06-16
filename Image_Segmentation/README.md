ce136a96b8858dbad98c865e58d273ba163fa652
Cecília Barbosa <105225682+barbo347@users.noreply.github.com>
2023-06-16 14:43:15 -0300

    Update readme.md

---
+++ Image_Segmentation/README.md
@@ -0,0 +1,24 @@
+# Human Segmentation Dataset
+
+Código em Python que utiliza o conjunto de dados de segmentação humana para treinar um modelo de segmentação de imagens.
+
+## Objetivos
+
+- Importação das bibliotecas necessárias.
+- Configuração das variáveis, como caminho do arquivo CSV, diretório dos dados, dispositivo de execução, número de épocas, taxa de aprendizado, tamanho das imagens, modelo de codificação e
+pesos pré-treinados.
+- Leitura do arquivo CSV e visualização de uma imagem e máscara.
+- Divisão dos dados em conjunto de treinamento e conjunto de validação.
+- Definição das funções de aumento de dados para treinamento e validação.
+- Criação de uma classe para o conjunto de dados personalizado.
+- Criação dos conjuntos de dados de treinamento e validação.
+- Exibição de uma imagem e máscara do conjunto de treinamento.
+- Carregamento dos dados em lotes usando DataLoader.
+- Criação do modelo de segmentação.
+- Definição da função de treinamento.
+- Definição da função de avaliação.
+- Treinamento do modelo e salvamento do melhor modelo com base na perda de validação.
+- Inferência em uma imagem do conjunto de validação e exibição da imagem original, máscara verdadeira e máscara predita.
+
+
+vxvd
--- Image_Segmentation/app.py
+++ Image_Segmentation/app.py
@@ -14,35 +14,39 @@ from torch.utils.data import Dataset
 import helper
 from torch import nn
 import segmentation_models_pytorch as smp
-from segmentation_models_pytorch.losses import Diceloss
+from segmentation_models_pytorch.losses import DiceLoss
 
-# Setup Configurations
+# Configurações
 
-CSV_FILE = "/Human-Segmentation-Dataset-master/train.csv"
-DATA_DIR = "/Image_Segmentation/"
+CSV_FILE = "/Human-Segmentation-Dataset-master/train.csv"  # Caminho para o arquivo CSV contendo informações sobre as imagens e máscaras.
+DATA_DIR = "/Image_Segmentation/"  # Diretório onde as imagens e máscaras estão armazenadas.
 
-DEVICE = "CUDA"
+DEVICE = "CUDA"  # Dispositivo de execução (por exemplo, "cuda" para usar a GPU).
 
-EPOCHS = 25
-LR = 0.003
-IMG_SIZE = 320
+EPOCHS = 25  # Número de épocas de treinamento.
+LR = 0.003  # Taxa de aprendizado.
+IMG_SIZE = 320  # Tamanho das imagens.
 
-ENCODER = "time-efficientnet-b0"
-WEIGHTS = "imagenet"
+ENCODER = "time-efficientnet-b0"  # Modelo de codificação a ser usado.
+WEIGHTS = "imagenet"  # Pesos pré-treinados para o modelo de codificação.
 
+# Leitura do arquivo CSV contendo informações sobre as imagens e máscaras
 df = pd.read_csv(CSV_FILE)
 df.head()
 
+# Seleção de uma linha do DataFrame para visualização
 row = df.iloc[4]
 
 image_path = row.images
 mask_path = row.masks
 
+# Carregamento da imagem e máscara usando OpenCV
 image = cv2.imread(image_path)
 image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
 mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE) / 255.0
 
+# Exibição da imagem e máscara usando matplotlib
 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
 
 ax1.set_title("IMAGE")
@@ -51,24 +55,19 @@ ax1.imshow(image)
 ax2.set_title("GROUND TRUTH")
 ax2.imshow(mask, cmap="gray")
 
+# Divisão dos dados em conjunto de treinamento e conjunto de validação
 train_df, valid_df = train_test_split(df, test_size=0.2, random_state=42)
 
-# Augmentation Functions
-
-
+# Funções de aumento de dados para treinamento e validação
 def get_train_augs():
     return A.Compose(
         [A.Resize(IMG_SIZE, IMG_SIZE), A.HorizontalFlip(p=0.5), A.VerticalFlip(p=0.5)]
     )
 
-
 def get_valid_augs():
     return A.Compose([A.Resize(IMG_SIZE, IMG_SIZE)])
 
-
-# Create Custom Dataset
-
-
+# Classe do conjunto de dados personalizado
 class SegmentationDataset(Dataset):
     def __init__(self, df, augmentations):
         self.df = df
@@ -86,15 +85,13 @@ class SegmentationDataset(Dataset):
         image = cv2.imread(image_path)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSACLE)  # (h, w, c)
-        mask = np.expand_dms(mask, axis=-1)
+        mask = cv2.imread(mask_path, cv2.IMREAD_GRAYSCALE)
+        mask = np.expand_dims(mask, axis=-1)
 
         if self.augmentations:
             data = self.augmentations(image=image, mask=mask)
             image = data["image"]
             mask = data["mask"]
-
-        # (h, w, c) -> (c, h, w)
 
         image = np.transpose(image, (2, 0, 1)).astype(np.float32)
         mask = np.transpose(mask, (2, 0, 1)).astype(np.float32)
@@ -104,22 +101,20 @@ class SegmentationDataset(Dataset):
 
         return image, mask
 
-
+# Criação dos conjuntos de dados de treinamento e validação
 trainset = SegmentationDataset(train_df, get_train_augs())
 validset = SegmentationDataset(valid_df, get_valid_augs())
 
 print(f"Size of Trainset : {len(trainset)}")
 print(f"Size of Validset : {len(validset)}")
 
+# Exemplo de visualização de uma imagem e máscara do conjunto de treinamento
 idx = 3
-
 image, mask = trainset[idx]
 helper.show_image(image, mask)
 
-# Load dataset into batches
-
+# Carregamento dos conjuntos de dados em lotes usando DataLoader
 BATCH_SIZE = 32
-
 trainloader = DataLoader(trainset, batch_size=BATCH_SIZE, shuffle=True)
 validloader = DataLoader(validset, batch_size=BATCH_SIZE)
 
@@ -132,9 +127,7 @@ for image, mask in trainloader:
 print(f"One batch image shape: {image.shape}")
 print(f"One batch mask shape : {mask.shape}")
 
-# Create Segmentation Model
-
-
+# Criação do modelo de segmentação
 class SegmentationModel(nn.Module):
     def __init__(self):
         super(SegmentationModel, self).__init__()
@@ -150,20 +143,17 @@ class SegmentationModel(nn.Module):
     def forward(self, images, masks=None):
         logits = self.arc(images)
 
-        if mask != None:
+        if masks is not None:
             loss1 = DiceLoss(mode="binary")(logits, masks)
             loss2 = nn.BCEWithLogitsLoss()(logits, masks)
             return logits, loss1 + loss2
 
         return logits
 
-
 model = SegmentationModel()
 model.to(DEVICE)
 
-# Create Train and Validation Function
-
-
+# Função de treinamento
 def train_fn(data_loader, model, optimizer):
     model.train()
     total_loss = 0.0
@@ -179,9 +169,9 @@ def train_fn(data_loader, model, optimizer):
 
         total_loss += loss.item()
 
-        return total_loss / len(data_loader)
+    return total_loss / len(data_loader)
 
-
+# Função de avaliação
 def eval_fn(data_loader, model):
     model.eval()
     total_loss = 0.0
@@ -195,13 +185,10 @@ def eval_fn(data_loader, model):
 
             total_loss += loss.item()
 
-        return total_loss / len(data_loader)
+    return total_loss / len(data_loader)
 
-
-# Train Model
-
+# Treinamento do modelo
 optimizer = torch.optim.Adam(model.parameters(), lr=LR)
-
 best_valid_loss = np.Inf
 
 for i in range(EPOCHS):
@@ -209,21 +196,18 @@ for i in range(EPOCHS):
     valid_loss = eval_fn(validloader, model)
 
     if valid_loss < best_valid_loss:
-        torch.save(model.state.dict(), "best_model.pt")
+        torch.save(model.state_dict(), "best_model.pt")
         print("SAVED-MODEL")
         best_valid_loss = valid_loss
 
     print(f"Epoch : {i+1} Train_loss : {train_loss} Valid_loss : {valid_loss}")
 
-# Inference
-
+# Inferência
 idx = 20
-
 model.load_state_dict(torch.load("/content/best_model.pt"))
 
 image, mask = validset[idx]
-
-logits_mask = model(image.to(DEVICE).unsqueeze(0))  # (C, H, W) -> (1, C, H, W)
+logits_mask = model(image.to(DEVICE).unsqueeze(0))
 pred_mask = torch.sigmoid(logits_mask)
 pred_mask = (pred_mask > 0.5) * 1.0
 
